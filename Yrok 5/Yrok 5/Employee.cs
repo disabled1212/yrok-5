e48d68585e7c328473747d467ca15c23daa2d318
daniil <100211314+disabled1212@users.noreply.github.com>
2023-06-04 20:22:30 +0300

    5

---
+++ Yrok 5/Yrok 5/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yrok_5
+{
+    internal class Employee
+    {
+        public string _fullName;
+        public int _department;
+        public double _salary;
+        public int _id;
+
+        public Employee(string fullname, int department, double salary, int id)
+        {
+            _fullName = fullname;
+            _department = department;
+            _salary = salary;
+            _id = id;
+
+        }
+        public string FullName
+        {
+            get => _fullName;
+            set => _fullName = value;
+        }
+        public int Department
+        {
+            get => _department;
+            set => _department = value;
+        }
+        public double Salary
+        {
+            get => _salary;
+            set => _salary = value;
+        }
+        public int ID
+        {
+            get => _id;
+            set => _id = value;
+        }
+    }
+}
+
